6dc6604322e281c4781c54f698aba9fa44f20509
AmanS1001 <amanpreet100190@gmail.com>
2022-10-05 23:01:09 -0400

    Assignment week 5

--- src/ChoclateBox.java
+++ src/ChoclateBox.java
@@ -4,22 +4,17 @@ public class ChoclateBox {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int maxBoxCapacity = 63;
-		int currentChoclates= 27;
+		int currentChoclates = 27;
 		int addChoclate = 5;
-	
-
-		for (
-				int counter = currentChoclates; 
-				counter < maxBoxCapacity; 
-				counter += addChoclate) 
-		{
-			currentChoclates = currentChoclates + addChoclate;
-
+		for (int counter = currentChoclates; counter <= maxBoxCapacity; counter += addChoclate) {
 			System.out.println("Number of Choclates in box are " + currentChoclates);
-			if (currentChoclates == maxBoxCapacity) {
-				System.out.println("No more choclates to be added");
+			currentChoclates = currentChoclates + addChoclate;
+			if (currentChoclates > maxBoxCapacity) {
+				System.out.println("Box reached its max capacity");
 				break;
 			}
-		}
+		}if (currentChoclates == maxBoxCapacity)
+		{System.out.println("Total number of choclates"  + currentChoclates + " = " + currentChoclates +1);
 	}
+}
 }
