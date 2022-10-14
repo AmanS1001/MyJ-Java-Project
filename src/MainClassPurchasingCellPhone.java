d635be45f064eba27a587d5248009e0974d1f887
AmanS1001 <amanpreet100190@gmail.com>
2022-10-14 09:42:49 -0400

    Assignment 6

--- src/MainClassPurchasingCellPhone.java
+++ src/MainClassPurchasingCellPhone.java
@@ -22,29 +22,6 @@ public class MainClassPurchasingCellPhone {
 			break;
 		}
 		System.out.println("Please enter the model that you want to buy: ");
-		String modelSelected =sc.next();
-		//Creating an instance of class
-		SelectMobilePhone selectMP = new SelectMobilePhone();
-		String modelTobePurchased = selectMP.modelType(modelSelected);
-		switch (modelTobePurchased) {
-		case "S22":
-			System.out.println("Model slected is S22 and price is $25/month");
-			break;
-		case "S22 plus":
-			System.out.println("Model slected is S22plus and price is $35/month");
-			break;
-		case "S22 Ultra":
-			System.out.println("Model slected is S22 Ultra and price is $55/month");
-			break;
-		case "S22 FE":
-			System.out.println("Model slected is S22FE and price is $20/month");
-			break;
-			default:
-				System.out.println("Invalid Input");
-				break;
-		}
-			
-		
 		
 		
 		
